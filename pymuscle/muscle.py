6c7c32c16866f70d42a9608f6f3e57a34071cc7d
Ian Danforth <iandanforth@gmail.com>
2018-05-14 15:52:26 -0700

    Work through import env issues

--- pymuscle/muscle.py
+++ pymuscle/muscle.py
@@ -5,4 +5,4 @@ class Muscle(object):
 
     def __init__(self, motor_unit_count):
         self.motor_unit_count = motor_unit_count
-        self.state_array = np.array(self.motor_unit_count)
+        self.state_array = np.zeros((1, self.motor_unit_count))
