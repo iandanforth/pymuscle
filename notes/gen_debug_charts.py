Fixed some off by one errors, but chart still bad

notes/gen_debug_charts.py
@@ -123,7 +123,7 @@ if False:
         plot(fig, filename='adapted-firing-rates.html')
 
 # Muscle Fiber Charts
-if True:
+if False:
     # Peak Twitch Forces
     fig = go.Figure(
         data=[go.Scatter(
@@ -148,6 +148,20 @@ if True:
     )
     plot(fig, filename='contraction-times.html')
 
+    # Fig 1.B
+    rel_twitch_force = fibers._peak_twitch_forces / fibers._peak_twitch_forces[0]
+    fig = go.Figure(
+        data=[go.Scatter(
+            x=fibers._contraction_times,
+            y=rel_twitch_force,
+            mode='markers'
+        )],
+        layout=go.Layout(
+            title='Relative Contraction Times'
+        )
+    )
+    plot(fig, filename='relative-contraction-times.html')
+
     # Nominal Fatigabilities
     fig = go.Figure(
         data=[go.Scatter(
@@ -165,7 +179,8 @@ if True:
     fig = go.Figure(
         data=[go.Scatter(
             x=rel_twitch_force,
-            y=fibers._nominal_fatigabilities
+            y=fibers._nominal_fatigabilities,
+            mode='markers'
         )],
         layout=go.Layout(
             title='Relative Fatigabilities'
@@ -174,7 +189,7 @@ if True:
     plot(fig, filename='relative-fatigabilities.html')
 
 # Force Charts
-if False:
+if True:
     xs = np.arange(0.0, 70.0, 0.1)
     forces = []
     all_forces_by_excitation = []

notes/target_force_sims.py
@@ -28,15 +28,13 @@ def get_force(excitations):
 
 
 # Target Force - 20% MVC
-max_force = 2234.0
+max_force = 2216.0
 max_excitation = 67.0
 target_percent = 20
 target_force = max_force * (target_percent / 100)
-excitations = np.full(motor_unit_count, 1.0)
 e_inc = 0.01
-_, _, total_force = get_force(excitations)
 sim_time = 0.0
-sim_duration = 200.0
+sim_duration = 550.0
 time_inc = 0.1
 force_capacities = fibers._peak_twitch_forces
 total_peak_capacity = sum(force_capacities)
@@ -47,7 +45,10 @@ all_excitation_levels = []
 all_total_capacities = []
 current_forces = []
 hit_max_excite = False
+excitations = np.full(motor_unit_count, 1.0)
 while sim_time < sim_duration:
+    total_force = 0.0
+    excitations = excitations - (20 * e_inc) # Start the search again from a slightly lower value
     # Find the required excitation level for target_force
     while total_force < target_force:
         # print(excitations[0])
@@ -56,8 +57,8 @@ while sim_time < sim_duration:
                 print("Hit max excitation: ", sim_time)
                 hit_max_excite = True
             break
-        excitations += e_inc
         _, _, total_force = get_force(excitations)
+        excitations += e_inc
 
     # We're now at the correct excitation level to generate target_force
     normalized_forces, current_forces, total_force = get_force(excitations)

pymuscle/potvin_muscle_fibers.py
@@ -67,6 +67,9 @@ class PotvinMuscleFibers(Model):
             self._peak_twitch_forces
         )
 
+        # These will change with fatigue
+        self._current_contraction_times = self._contraction_times
+
         self._nominal_fatigabilities = self._calc_nominal_fatigabilities(
             motor_unit_count,
             fatigability_range,
@@ -80,10 +83,15 @@ class PotvinMuscleFibers(Model):
         self.motor_unit_count = motor_unit_count
 
     def _apply_fatigue(self, inst_fatigues: ndarray, step_size: float) -> None:
+        # Update twitch forces
         fatigues = step_size * inst_fatigues
         self._current_twitch_forces -= fatigues
         # Zero out negative values
         self._current_twitch_forces[self._current_twitch_forces < 0] = 0.0
+
+        # Update contraction times
+        # increase_percents = self._calc_ct_increase_percents(fatigues)
+        # self._current_contraction_times *= (1 + increase_percents)
 
     @staticmethod
     def _calc_contraction_times(
@@ -120,7 +128,7 @@ class PotvinMuscleFibers(Model):
         """
         motor_unit_indices = np.arange(1, motor_unit_count + 1)
         t_log = np.log(max_twitch_amplitude)
-        t_exponent = (t_log * (motor_unit_indices)) / (motor_unit_count)
+        t_exponent = (t_log * (motor_unit_indices - 1)) / (motor_unit_count - 1)
         return np.exp(t_exponent)
 
     @staticmethod
@@ -134,13 +142,18 @@ class PotvinMuscleFibers(Model):
         """
         motor_unit_indices = np.arange(1, motor_unit_count + 1)
         f_log = np.log(fatigability_range)
-        f_exponent = (f_log * (motor_unit_indices)) / (motor_unit_count)
-        return np.exp(f_exponent) * fatigue_factor_first_unit
+        f_exponent = (f_log * (motor_unit_indices - 1)) / (motor_unit_count - 1)
+        return fatigue_factor_first_unit * np.exp(f_exponent)
 
     def _normalize_firing_rates(self, firing_rates: ndarray) -> ndarray:
         # Divide by 1000 here as firing rates are per second where contraction
         # times are in milliseconds.
         return (firing_rates / 1000) * self._contraction_times
+
+    def _normalize_firing_rates_w_fatigue(self, firing_rates: ndarray) -> ndarray:
+        # Divide by 1000 here as firing rates are per second where contraction
+        # times are in milliseconds.
+        return (firing_rates / 1000) * self._current_contraction_times
 
     @staticmethod
     def _calc_normalized_forces(normalized_firing_rates: ndarray) -> ndarray:
@@ -179,7 +192,7 @@ class PotvinMuscleFibers(Model):
         """
         return normalized_forces * self._nominal_fatigabilities
 
-    def _calc_contraction_time_increase(self, force_losses: ndarray):
+    def _calc_ct_increase_percents(self, force_losses: ndarray):
         """
         Calculates the percent that contraction times should increase for
         a given loss in force.
@@ -201,7 +214,7 @@ class PotvinMuscleFibers(Model):
         Calculates the total instantaneous force produced by all fibers for
         the given instantaneous firing rates.
         """
-        normalized_firing_rates = self._normalize_firing_rates(firing_rates)
+        normalized_firing_rates = self._normalize_firing_rates_w_fatigue(firing_rates)
         normalized_forces = self._calc_normalized_forces(normalized_firing_rates)
         inst_forces = self._calc_inst_forces(normalized_forces)
         return self._calc_total_inst_force(inst_forces)