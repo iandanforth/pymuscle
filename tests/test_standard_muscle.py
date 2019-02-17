Correct calculation of max_arb_output in StandardMuscle and update tests accordingly; Add tests for pymuscle_fibers

tests/test_standard_muscle.py
@@ -11,7 +11,7 @@ def test_init():
     assert m.motor_unit_count == 120
 
     # Check calculated max_output
-    max_output = 2601.6260
+    max_output = 2609.0309
     assert m.max_arb_output == pytest.approx(max_output)
 
     max_force = 90.0
@@ -19,7 +19,7 @@ def test_init():
 
     # Check calculated number of motor units
     assert m.motor_unit_count == 340
-    max_output = 7317.07317
+    max_output = 7338.29062
     assert m.max_arb_output == pytest.approx(max_output)
 
 
@@ -40,7 +40,7 @@ def test_step():
     # Moderate
     m = Muscle(max_force)
     moderate_input = 0.5
-    moderate_output = 0.392076
+    moderate_output = 0.39096348
     output = m.step(np.full(m.motor_unit_count, moderate_input), 1.0)
     assert output == pytest.approx(moderate_output)
 
@@ -52,7 +52,7 @@ def test_step():
     # Max
     m = Muscle(max_force)
     max_input = 1.0
-    max_output = 0.851767
+    max_output = 0.84935028
     output = m.step(np.full(m.motor_unit_count, max_input), 1.0)
     assert output == pytest.approx(max_output)
 
@@ -66,16 +66,49 @@ def test_fatigue():
     # With fatigue off the return should always be the same
     max_force = 32.0
     m = Muscle(max_force, apply_peripheral_fatigue=False)
+
+    # As measured by fatigue
+    fatigue_before = m.get_peripheral_fatigue()
+    assert fatigue_before == 0.0
+
+    # As measured by output
     moderate_input = 0.5
-    moderate_output = 0.392076
+    moderate_output = 0.39096348
     for i in range(100):
         output = m.step(moderate_input, 1.0)
-        assert output == pytest.approx(moderate_output)
+    assert output == pytest.approx(moderate_output)
 
-    # Fatigue on you should see decreasing output over time
+    # And again by fatigue after
+    fatigue_after = m.get_peripheral_fatigue()
+    assert fatigue_after == 0.0
+
+    # Fatigue ON
+
+    # You should see no change with zero activation
     m = Muscle(max_force, apply_peripheral_fatigue=True)
-    fatigued_output = 0.292348
+
+    # As measured by fatigue
+    fatigue_before = m.get_peripheral_fatigue()
+    assert fatigue_before == 0.0
+
+    for i in range(100):
+        output = m.step(0.0, 1.0)
+    assert output == pytest.approx(0.0)
+
+    # As measured by fatgue after
+    fatigue_after = m.get_peripheral_fatigue()
+    assert fatigue_after == 0.0
+
+    # You should see increasing fatigue and decreasing output
+    # with non-zero inputs.
+
+    # Measured by output
+    fatigued_output = 0.29151827
     for i in range(100):
         output = m.step(moderate_input, 1.0)
-
     assert output == pytest.approx(fatigued_output)
+
+    # As measured by fatgue after
+    expected_fatigue = 0.18028181
+    fatigue_after = m.get_peripheral_fatigue()
+    assert pytest.approx(fatigue_after, expected_fatigue)